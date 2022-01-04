69c9eab41268f7afa158738d2851f8815de8d6f3
Glitch (hello-express) <none>
2022-01-04 13:28:36 +0000

    🎊🍕 Checkpoint ./lib/definitions.js:28824481/766 ./server.js:28824481/104

--- server.js
+++ server.js
@@ -833,7 +833,7 @@ const skcnv = {
     rgn: 8,
     mob: 9,
 };
-const levelers = [
+const levelers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
      44,
 ];
 class Skill {
