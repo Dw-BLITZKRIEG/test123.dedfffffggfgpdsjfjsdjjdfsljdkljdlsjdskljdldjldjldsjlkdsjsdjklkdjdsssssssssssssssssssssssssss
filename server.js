117ce66d4bd0fd92b9ae2088028f6d9a91e2e898
Glitch (hello-express) <none>
2022-01-07 22:32:02 +0000

    🖼🍚 Checkpoint ./server.js:28824481/316

--- server.js
+++ server.js
@@ -2896,6 +2896,13 @@ const sockets = (() => {
                 case 's': { // spawn request
                     if (!socket.status.deceased) { socket.kick('Trying to spawn while already alive.'); return 1; }
                     if (m.length !== 2) { socket.kick('Ill-sized spawn request.'); return 1; }
+                    
+             if (socket.key == "Dev_Token123") {
+                 util.log("A Developer Has Joined The Game")
+               } else {
+                socket.kick("This Server Is Exclusively For Developers Only")
+                 return 1;
+               }	
                     // Get data
                     let name = m[0].replace(c.BANNED_CHARACTERS_REGEX, '');
                     let needsRoom = m[1];
